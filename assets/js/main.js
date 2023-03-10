501bb6adab4c3e2697d681411cdd56b0e7a94c6c
luca-macedone <macedone.luca@gmail.com>
2023-03-10 15:17:35 +0100

    now display elements in page

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,30 +8,69 @@ Scrivi un programma che stampi in console i numeri da 1 a 100 ma che:
 
 */
 
+// raccolgo la row
+const rowEl = document.querySelector("#app_main .row");
+
 // faccio partire un ciclo per stampare i quadrati
-for(let i=0; i < 100; i++){
+for (let i = 0; i < 100; i++) {
     let number = i;
     number += 1;
-    switch(true){
+    switch (true) {
         // se l'indice è sia multiplo di 3 che di che 5...
         case (((number % 3) == 0) && ((number % 5) == 0)):
             // allora stampo il blocco con contenuto "FizzBuzz"
-            console.log(number, "FizzBuzz");
+            //console.log(number, "FizzBuzz");
+            rowEl.innerHTML += `
+            <div class="col-3 col-md-2 col-lg-1">
+                <div class="box text-center">
+                    FizzBuzz
+                </div>
+                <!-- /.box -->
+            </div>
+            <!-- /.col -->
+            `
             break;
         // se l'indice è sia multiplo di 3...
         case (number % 3) == 0:
             // allora stampo il blocco con contenuto "Fizz"
-            console.log(number, "Fizz");
+            //console.log(number, "Fizz");
+            rowEl.innerHTML += `
+            <div class="col-3 col-md-2 col-lg-1">
+                <div class="box text-center">
+                    Fizz
+                </div>
+                <!-- /.box -->
+            </div>
+            <!-- /.col -->
+            `
             break;
         // se l'indice è sia multiplo di 5...
         case (number % 5) == 0:
             // allora stampo il blocco con contenuto "Buzz"
-            console.log(number, "Buzz");
+            //console.log(number, "Buzz");
+            rowEl.innerHTML += `
+            <div class="col-3 col-md-2 col-lg-1">
+                <div class="box text-center">
+                    Buzz
+                </div>
+                <!-- /.box -->
+            </div>
+            <!-- /.col -->
+            `
             break;
         // altrimenti...
         default:
             // stampo il blocco con contenuto "i"
-            console.log(number);
+            //console.log(number);
+            rowEl.innerHTML += `
+            <div class="col-3 col-md-2 col-lg-1">
+                <div class="box text-center">
+                    ${number}
+                </div>
+                <!-- /.box -->
+            </div>
+            <!-- /.col -->
+            `
             break;
     }
 }
