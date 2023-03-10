00be29494904f5c988e7809789c82f6c64b5a5d1
luca-macedone <macedone.luca@gmail.com>
2023-03-10 14:44:48 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+/*
+
+Consegna:
+Scrivi un programma che stampi in console i numeri da 1 a 100 ma che:
+- per i multipli di 3 stampi “Fizz” al posto del numero
+- per i multipli di 5 stampi “Buzz”
+- per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+*/
